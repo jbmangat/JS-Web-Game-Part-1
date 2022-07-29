dd4e87bf1f6e9f2f6bc0f95aaec813b0d54b020a
Jagbir Mangat <jaguarsandhu@gmail.com>
2022-07-29 14:20:43 -0700

    fixed typo in name of shield.png asset. created function for andding images and assets

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(asset, left, bottom) {
+    let greenCharater = document.createElement('img')
+    greenCharater.src = asset
+    greenCharater.style.position = 'fixed'
+    greenCharater.style.left = left
+    greenCharater.style.bottom = bottom
+    document.body.append(greenCharater)
+}
+
+newImage('assets/green-character.gif', '100px', '100px')
+newImage('assets/pine-tree.png', '450px', '200px')
+newImage('assets/tree.png', '200px', '300px')
+newImage('assets/pillar.png', '350px', '100px')
+newImage('assets/crate.png', '150px', '200px')
+newImage('assets/well.png', '500px', '425px')
+
+function newItem(itemAsset, left, bottom){
+    let item = document.createElement('img')
+    item.src = itemAsset
+    item.style.position = 'fixed'
+    item.style.left = left
+    item.style.bottom = bottom
+    document.body.append(item)
+
+    item.addEventListener('dblclick', function(){
+        item.remove()
+    })
+}
+
+newItem('assets/sword.png', '500px', '405px')
+newItem('assets/shield.png', '165px', '100px')
+newItem('assets/staff.png', '600px', '100px')
